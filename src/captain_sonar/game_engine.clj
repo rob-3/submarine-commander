77280bb14c77c5073b0dc59d42631c353087d6fc
Robert Boyd III <git@robertboyd.dev>
2024-05-23 21:12:23 -0400

    Create template for game state

---
+++ src/captain_sonar/game_engine.clj
@@ -0,0 +1,26 @@
+(ns captain-sonar.game-engine)
+
+(def state
+  {:red {:location [1 1]
+         :systems {:torpedo 0
+                   :mine 0
+                   :drone 0
+                   :sonar 0
+                   :silence 0}
+         ;; the presence of a keyword means the box is marked
+         :breakdowns {:west #{:red :yellow :green1 :green2 :reactor1 :reactor2}
+                      :north #{:yellow1 :yellow2 :red1 :green1 :red2 :reactor}
+                      :south #{:green :yellow1 :red1 :red2 :reactor :yellow2}
+                      :east #{:green1 :yellow :red :reactor :green2 :reactor2}}
+         :surfaced false}
+   :blue {:location [15 15]
+          :systems {:torpedo 0
+                    :mine 0
+                    :drone 0
+                    :sonar 0
+                    :silence 0}
+          :breakdowns {:west #{:red :yellow :green1 :green2 :reactor1 :reactor2}
+                       :north #{:yellow1 :yellow2 :red1 :green1 :red2 :reactor}
+                       :south #{:green :yellow1 :red1 :red2 :reactor :yellow2}
+                       :east #{:green1 :yellow :red :reactor :green2 :reactor2}}
+          :surfaced false}})
