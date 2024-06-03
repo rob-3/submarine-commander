0bd6bbefdcd15886c498bcda381c18e325807a25
Robert Boyd III <git@robertboyd.dev>
2024-06-03 15:05:53 -0400

    Add page for a randomly generated room

--- src/captain_sonar/captain_sonar.clj
+++ src/captain_sonar/captain_sonar.clj
@@ -1,8 +1,11 @@
 (ns captain-sonar.captain-sonar
   (:gen-class)
-  (:require [ring.adapter.jetty :as jetty]
-            [hiccup2.core :as h]
-            [hiccup.page :as page]))
+  (:require
+   [clojure.walk :refer [keywordize-keys]]
+   [hiccup.page :as page]
+   [hiccup2.core :as h]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.codec :refer [form-decode]]))
 
 (defmacro html [& args]
   `(str (h/html ~@args)))
@@ -10,23 +13,62 @@
 (defn file-rsp [f]
   {:status 200 :headers {} :body (slurp f)})
 
-(defn index []
-  (page/html5 (page/include-css "/index.css")
-              [:script {:type "text/javascript"
-                        :src "https://unpkg.com/htmx.org@1.9.12"
-                        :integrity "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
-                        :crossorigin "anonymous"}]
-              (h/html [:div.container
-                        [:h1.title "Captain Sonar"]
-                        [:button.button {:hx-get "/create-room"
-                                         :hx-swap "outerHTML"} "Create a room"]])))
+(defn query-params [request]
+ (-> request :query-string form-decode keywordize-keys))
+
+(defn index [uuid]
+  (let [url (str "/room?id=" uuid)]
+    [:div.container
+        [:h1.title "Captain Sonar"]
+        [:button {:hx-get url
+                  :hx-push-url "true"
+                  :hx-swap "outerHTML"} "Create a room"]]))
+
+(defn request-type [r]
+  (let [headers (:headers r)
+        htmx? (get headers "hx-request")]
+    (if (= htmx? "true")
+      :htmx
+      :default)))
+
+(ns-unmap *ns* 'index-page)
+(defmulti index-page request-type)
+(defmethod index-page :default [_request]
+  (let [uuid (random-uuid)]
+    (page/html5 (page/include-css "/index.css")
+                [:script {:type "text/javascript"
+                          :src "https://unpkg.com/htmx.org@1.9.12"
+                          :integrity "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
+                          :crossorigin "anonymous"}]
+                (h/html (index uuid)))))
+
+(defn room [uuid]
+  (str (h/html [:div (str "Room " uuid)])))
+
+(ns-unmap *ns* 'room-handler)
+(defmulti room-handler request-type)
+(defmethod room-handler :htmx [request]
+  (def request request)
+  (let [uuid (:id (query-params request))] 
+    (room uuid)))
+
+(defmethod room-handler :default [request]
+  (let [uuid (:id (query-params request))] 
+    (page/html5 (page/include-css "/index.css")
+                [:script {:type "text/javascript"
+                          :src "https://unpkg.com/htmx.org@1.9.12"
+                          :integrity "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
+                          :crossorigin "anonymous"}]
+                (h/html [:div.container
+                         [:h1.title "Captain Sonar"]
+                         (room uuid)]))))
 
 (defn app [request]
   (case [(:request-method request) (:uri request)]
-    [:get "/"] {:status 200 :headers {} :body (index)}
+    [:get "/"] {:status 200 :headers {} :body (index-page request)}
     [:get "/index.css"] (file-rsp "resources/index.css")
     [:post "/"] {:status 200 :headers {} :body "post"}
-    [:get "/create-room"] {:status 200 :headers {} :body (html [:h1 "TODO"])}
+    [:get "/room"] {:status 200 :headers {} :body (str (room-handler request))}
     {:status 404 :headers {} :body (html [:h1 "404 Not Found"])}))
 
 (defn -main
