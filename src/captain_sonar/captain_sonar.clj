8fcdddc6656bac5bedb072012d1116b226d3ef86
Robert Boyd III <git@robertboyd.dev>
2024-06-07 18:29:59 -0400

    Set auth cookies and admin status

--- src/captain_sonar/captain_sonar.clj
+++ src/captain_sonar/captain_sonar.clj
@@ -5,6 +5,7 @@
    [hiccup.page :as page]
    [hiccup2.core :as h]
    [ring.adapter.jetty :as jetty]
+   [ring.middleware.cookies :as middleware]
    [ring.util.codec :refer [form-decode]]
    [ring.websocket :as ws]))
 
@@ -47,27 +48,38 @@
   (let [uuid (random-uuid)]
     (page-skeleton (index uuid))))
 
-(defn room [uuid]
+;; FIXME we should probably maintain a user data map that allows user->room too
+;; That way we can prevent people from joining multiple rooms by mistake.
+(def rooms (atom {}))
+
+(defn room-html [uuid admin?]
   [:div#ws {:hx-ext "ws" :ws-connect "/ws"}
    [:div (str "Room " uuid)]
+   (when admin? [:div (str "You are the admin.")])
    [:div {:hx-trigger "click" :ws-send "" :id "howdy"} "hi"]
    [:div#messages]
    [:button {:hx-get "/delete" :hx-target "#ws" :hx-swap "outerHTML"} "Delete"]])
 
+(defn join-room [room player]
+  (swap! rooms update room (fnil #(update % :players conj player)
+                                 {:admin player :players #{player}})))
+
 (ns-unmap *ns* 'room-handler)
 (defmulti room-handler request-type)
 (defmethod room-handler :htmx [request]
-  (let [uuid (:id (query-params request))]
-    (h/html (room uuid))))
+  (let [room (:id (query-params request))
+        player (:value (get (:cookies request) "id"))]
+    (swap! rooms update room (fnil #(update % :players conj player)
+                                   {:admin player :players #{player}}))
+    (h/html (room-html room (= player (get-in @rooms [room :admin]))))))
 
 (defmethod room-handler :default [request]
-  (let [uuid (:id (query-params request))]
+  (let [room (:id (query-params request))
+        player (:value (get (:cookies request) "id"))]
+    (join-room room player)
     (page-skeleton [:div.container
                     [:h1.title "Captain Sonar"]
-                    (room uuid)])))
-
-(defn extract-token [auth-string]
-  (second (re-find #"^Bearer (.*)" auth-string)))
+                    (room-html room (= player (get-in @rooms [room :admin])))])))
 
 (def sockets (atom {}))
 (defn ws-handler [request]
@@ -76,9 +88,8 @@
      {:on-open (fn [socket]
                  (println (:remote-addr request) "connected")
                  (ws/send socket (html [:div#messages {:hx-swap-oob "beforeend"} [:div "Howdy!"]]))
-                 (let [auth-header (or (get-in request [:headers "Authorization"]) "")
-                       token (extract-token auth-header)]
-                   (swap! sockets assoc socket token))
+                 (let [token (or (get-in request [:cookies "id" :value]) "")]
+                   (swap! sockets assoc token socket))
                  (let [keep-alive (fn []
                                     (while (ws/open? socket)
                                       (ws/ping socket)
@@ -94,7 +105,15 @@
     [:get "/"] {:status 200 :headers {"vary" "hx-request"} :body (index-page request)}
     [:get "/index.css"] (file-rsp "resources/index.css")
     [:post "/"] {:status 200 :headers {"vary" "hx-request"} :body "post"}
-    [:get "/room"] {:status 200 :headers {"vary" "hx-request"} :body (str (room-handler request))}
+    [:get "/room"] (merge {:status 200 :headers {"vary" "hx-request"} :body (str (room-handler request))}
+                          (when-not (get-in request [:cookies "id"])
+                            {:cookies {"id"
+                                       {:value (str (random-uuid))
+                                        :secure true
+                                        :http-only true
+                                        :same-site :strict
+                                        :max-age 86400
+                                        :path "/"}}}))
     [:get "/delete"] {:status 200 :headers {"vary" "hx-request"} :body ""}
     [:get "/ws"] (ws-handler request)
     {:status 404 :headers {"vary" "hx-request"} :body (html [:h1 "404 Not Found"])}))
@@ -102,7 +121,7 @@
 (defn -main
   "Start here!"
   [& _args]
-  (jetty/run-jetty #(app %) {:port 3000 :join? false}))
+  (jetty/run-jetty (middleware/wrap-cookies #(app %)) {:port 3000 :join? false}))
 
 (comment
   (-main)
