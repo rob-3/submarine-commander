da5582885eca65ef7dfd4946bb33492b472144b0
Robert Boyd III <git@robertboyd.dev>
2024-06-07 21:16:34 -0400

    Use core.async channels to prevent race conditions

--- src/captain_sonar/captain_sonar.clj
+++ src/captain_sonar/captain_sonar.clj
@@ -1,19 +1,21 @@
 (ns captain-sonar.captain-sonar
   (:gen-class)
   (:require
+   [cheshire.core :as json]
    [clojure.walk :refer [keywordize-keys]]
    [hiccup.page :as page]
    [hiccup2.core :as h]
    [ring.adapter.jetty :as jetty]
    [ring.middleware.cookies :as middleware]
    [ring.util.codec :refer [form-decode]]
-   [ring.websocket :as ws]))
+   [ring.websocket :as ws]
+   [clojure.core.async :refer [<!! >!!] :as a]))
 
 (defmacro html [& args]
   `(str (h/html ~@args)))
 
 (defn file-rsp [f]
-  {:status 200 :headers {"vary" "hx-request"} :body (slurp f)})
+  {:status 200 :headers {"vary" "hx-request" "cache-control" "no-store"} :body (slurp f)})
 
 (defn query-params [request]
   (-> request :query-string form-decode keywordize-keys))
@@ -40,7 +42,8 @@
                         :integrity "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
                         :crossorigin "anonymous"}]
               [:script {:src "https://unpkg.com/htmx.org@1.9.12/dist/ext/ws.js"}]
-              (h/html children)))
+              (h/html [:div#ws.container {:hx-ext "ws" :ws-connect "/ws"}
+                       children])))
 
 (ns-unmap *ns* 'index-page)
 (defmulti index-page request-type)
@@ -50,62 +53,126 @@
 
 ;; FIXME we should probably maintain a user data map that allows user->room too
 ;; That way we can prevent people from joining multiple rooms by mistake.
-(def rooms (atom {}))
+(defonce rooms (atom {}))
 
-(defn room-html [uuid admin?]
-  [:div#ws {:hx-ext "ws" :ws-connect "/ws"}
-   [:div (str "Room " uuid)]
-   (when admin? [:div (str "You are the admin.")])
-   [:div {:hx-trigger "click" :ws-send "" :id "howdy"} "hi"]
-   [:div#messages]
-   [:button {:hx-get "/delete" :hx-target "#ws" :hx-swap "outerHTML"} "Delete"]])
+(defn room-html [room-id player-id {:keys [admin players]}]
+  (let [in-room? (contains? players player-id)
+        admin? (= player-id admin)]
+    [:div#app.container
+     (when admin? [:div "You are the admin. (" player-id ")"])
+     (for [player players]
+       (if (= player player-id)
+         (when-not admin? [:div (str "You (" player ") are in the room.")])
+         [:div (str "Player " player " is in the room.")]))
+     (if in-room?
+       [:button.button {:hx-push-url "true" :hx-post "/leave-room"} "Leave Room"]
+       [:form {:ws-send "" :id "join-form"}
+        [:input {:type "hidden" :name "room" :value room-id}]
+        [:input {:type "text" :name "username" :required ""}]
+        [:button {:type "submit" :name "event" :value "join-room"} "Join room"]])]))
 
-(defn join-room [room player]
-  (swap! rooms update room (fnil #(update % :players conj player)
-                                 {:admin player :players #{player}})))
+(defn add-to-room [old room player] (update old room (fnil #(update % :players conj player)
+                                                           {:admin player :players #{player}})))
+
+(defn remove-from-all-rooms [rooms player]
+  (reduce (fn [acc [room-id {:keys [admin players]}]]
+            (let [players' (disj players player)
+                  admin' (if (= player admin) (first players') admin)]
+              (if admin' (assoc acc room-id {:admin admin' :players players'}) acc)))
+          {}
+          rooms))
+
+;; FIXME these are terrible names
+(defonce c (a/chan (a/sliding-buffer 100)))
+(defn linearize! [f]
+  (>!! c f))
+
+(defn join-room! [room player]
+  (swap! rooms #(-> %
+                    (remove-from-all-rooms player)
+                    (add-to-room room player))))
+
+(defn leave-room! [player]
+  (swap! rooms #(remove-from-all-rooms % player)))
 
 (ns-unmap *ns* 'room-handler)
 (defmulti room-handler request-type)
 (defmethod room-handler :htmx [request]
-  (let [room (:id (query-params request))
-        player (:value (get (:cookies request) "id"))]
-    (swap! rooms update room (fnil #(update % :players conj player)
-                                   {:admin player :players #{player}}))
-    (h/html (room-html room (= player (get-in @rooms [room :admin]))))))
+  (let [room-id (:id (query-params request))
+        player-id (:value (get (:cookies request) "id"))
+        rooms @rooms]
+    (h/html (room-html room-id player-id (get rooms room-id)))))
 
 (defmethod room-handler :default [request]
-  (let [room (:id (query-params request))
-        player (:value (get (:cookies request) "id"))]
-    (join-room room player)
+  (let [room-id (:id (query-params request))
+        player-id (:value (get (:cookies request) "id"))
+        rooms @rooms]
     (page-skeleton [:div.container
                     [:h1.title "Captain Sonar"]
-                    (room-html room (= player (get-in @rooms [room :admin])))])))
+                    (room-html room-id player-id (get rooms room-id))])))
 
-(def sockets (atom {}))
+(defonce users (atom {}))
+
+(defn on-message [_socket message player-id]
+  (let [{event "event" room-id "room"} (json/parse-string message)]
+    (prn message)
+    (case event
+      "join-room" (linearize! #(join-room! room-id player-id)))))
+
 (defn ws-handler [request]
   (if (ws/upgrade-request? request)
-    {::ws/listener
-     {:on-open (fn [socket]
-                 (println (:remote-addr request) "connected")
-                 (ws/send socket (html [:div#messages {:hx-swap-oob "beforeend"} [:div "Howdy!"]]))
-                 (let [token (or (get-in request [:cookies "id" :value]) "")]
-                   (swap! sockets assoc token socket))
-                 (let [keep-alive (fn []
-                                    (while (ws/open? socket)
-                                      (ws/ping socket)
-                                      (Thread/sleep 1000)))]
-                   (future (keep-alive))))
-      :on-pong (fn [_socket _buffer] (println (:remote-addr request) "pong"))
-      :on-message (fn [_socket message] (println message))
-      :on-close (fn [_socket _code _reason] (println (:remote-addr request) "disconnected"))}}
-    {:status 400 :headers {"vary" "hx-request"} :body "Websocket upgrade requests only!"}))
+    (let [user-id (or (get-in request [:cookies "id" :value]) "")]
+      {::ws/listener
+       {:on-open (fn [socket]
+                   (println (:remote-addr request) "connected")
+                   ;(ws/send socket (html [:div#messages {:hx-swap-oob "beforeend"} [:div "Howdy!"]]))
+                   (let [current-time (System/currentTimeMillis)]
+                     (swap! users assoc user-id {:socket socket :last-ping current-time}))
+                   (let [keep-alive (fn []
+                                      (while (ws/open? socket)
+                                        (ws/ping socket)
+                                        (Thread/sleep 1000)))]
+                     (future (keep-alive))))
+        :on-pong (fn [_socket _buffer]
+                   (println (:remote-addr request) "pong")
+                   (let [current-time (System/currentTimeMillis)]
+                     (swap! users assoc-in [user-id :last-ping] current-time)))
+        :on-message #(on-message %1 %2 user-id)}})
+        ;:on-close (fn [_socket _code _reason]
+        ;            (println (:remote-addr request) "disconnected")
+        ;            (println _reason)
+        ;            (let [player (get-in request [:cookies "id" :value])]
+        ;              (future ((fn []
+        ;                         (Thread/sleep 5000)
+        ;                         (let [last-ping (get-in @users [user-id :last-ping])
+        ;                               current-time (System/currentTimeMillis)]
+        ;                           (if (> (- current-time 3000) last-ping)
+        ;                             (do
+        ;                               (leave-room! player)
+        ;                               (println "left"))
+        ;                             (println "didn't leave")))))))}})
+    {:status 400 :headers {"vary" "hx-request" "cache-control" "no-store"} :body "Websocket upgrade requests only!"}))
+
+(defn index-handler [request]
+  {:status 200
+   :headers {"vary" "hx-request" "cache-control" "no-store"}
+   :body (index-page request)})
+
+(defn leave-room-handler [request]
+  (let [player-id (get-in request [:cookies "id" :value])]
+    (linearize! #(leave-room! player-id))
+    {:status 200
+     :headers {"vary" "hx-request"
+               "cache-control" "no-store"
+               "hx-redirect" "/"}
+     :body nil}))
 
 (defn app [request]
   (case [(:request-method request) (:uri request)]
-    [:get "/"] {:status 200 :headers {"vary" "hx-request"} :body (index-page request)}
+    [:get "/"] (index-handler request)
     [:get "/index.css"] (file-rsp "resources/index.css")
-    [:post "/"] {:status 200 :headers {"vary" "hx-request"} :body "post"}
-    [:get "/room"] (merge {:status 200 :headers {"vary" "hx-request"} :body (str (room-handler request))}
+    [:post "/"] {:status 200 :headers {"vary" "hx-request" "cache-control" "no-store"} :body "post"}
+    [:get "/room"] (merge {:status 200 :headers {"vary" "hx-request" "cache-control" "no-store"} :body (str (room-handler request))}
                           (when-not (get-in request [:cookies "id"])
                             {:cookies {"id"
                                        {:value (str (random-uuid))
@@ -114,13 +181,18 @@
                                         :same-site :strict
                                         :max-age 86400
                                         :path "/"}}}))
-    [:get "/delete"] {:status 200 :headers {"vary" "hx-request"} :body ""}
     [:get "/ws"] (ws-handler request)
-    {:status 404 :headers {"vary" "hx-request"} :body (html [:h1 "404 Not Found"])}))
+    [:post "/leave-room"] (leave-room-handler request)
+    {:status 404 :headers {"vary" "hx-request" "cache-control" "no-store"} :body (html [:h1 "404 Not Found"])}))
 
 (defn -main
   "Start here!"
   [& _args]
+  (a/thread
+    (loop []
+      (let [f (<!! c)]
+        (f)
+        (recur))))
   (jetty/run-jetty (middleware/wrap-cookies #(app %)) {:port 3000 :join? false}))
 
 (comment
